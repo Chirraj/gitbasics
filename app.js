c3ba3378200700ed33863b2be25b336c8b7a7ced
Chemmanuel1996 <chemmanuel1096@gmail.com>
2024-04-20 09:44:53 +0530

    add hungry check

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 // remove duplicates
 
 const duplicates = (arr) => {
-    const flattenedArr = arr.reduce((prev, curr) => {
-      for (let i = 0; i < curr.length; i++) {
-        const val = curr[i];
-        if (prev.indexOf(val) === -1 ) {
-          prev.push(val);
-        }
+  const flattenedArr = arr.reduce((prev, curr) => {
+    for (let i = 0; i < curr.length; i++) {
+      const val = curr[i];
+      if (prev.indexOf(val) === -1) {
+        prev.push(val);
       }
-      return prev;
-    }, []);
-    return flattenedArr;
-  };
-  
-function main(){
-  duplicates();   
+    }
+    return prev;
+  }, []);
+  return flattenedArr;
+};
+
+function main() {
+  duplicates();
   console.log("Main");
 }
 
-function main1(){
-    console.log("Main 1");
+function main1() {
+  console.log("Main 1");
+  const hungry = true;
+  if (hungry) {
+    console.log("eact pizza");
+  } else if (hungry === false) {
+    console.log("drink water");
+  } else {
+    console.log("Please run");
+  }
 }
-  console.log(duplicates([[1, 2, 3, 5], [3, 2, 1, 5], [97.87]]));
-  
+console.log(duplicates([[1, 2, 3, 5], [3, 2, 1, 5], [97.87]]));
