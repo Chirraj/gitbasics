6d15c7feb2e02f2904b4eeaefab79f50ce6478e6
Chemmanuel1996 <chemmanuel1096@gmail.com>
2024-04-20 06:31:09 +0530

    add new reduce function

---
+++ app.js
@@ -0,0 +1,17 @@
+// remove duplicates
+
+const duplicates = (arr) => {
+    const flattenedArr = arr.reduce((prev, curr) => {
+      for (let i = 0; i < curr.length; i++) {
+        const val = curr[i];
+        if (prev.indexOf(val) === -1 ) {
+          prev.push(val);
+        }
+      }
+      return prev;
+    }, []);
+    return flattenedArr;
+  };
+  
+  console.log(duplicates([[1, 2, 3, 5], [3, 2, 1, 5], [97.87]]));
+  
